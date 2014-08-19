7270473344a7ae8e2c07b4287747dfd8b5b2fb32
Remi C <remi.fake@fake.com>
2014-08-19 18:29:31 +0200

    workaround because the Z_origin contains the laser roatating motion.

--- Point_Cloud/Image_Porcessing/building_border_detection/detecting_wall_with_accum.py
+++ Point_Cloud/Image_Porcessing/building_border_detection/detecting_wall_with_accum.py
@@ -17,25 +17,13 @@ import numpy as np
 from time import gmtime, strftime
 
 #importing image :
-src_tif = '/media/sf_E_RemiCura/PROJETS/Postgres_Day_2014_10_RemiC/Data/rasterized_pointcloud_max_height/raster_1.tif'
+src_tif = '/media/sf_E_RemiCura/PROJETS/Postgres_Day_2014_10_RemiC/Data/rasterized_pointcloud_min_height/raster_1_all_attributes_min.tif'
 dest_folder = '/media/sf_E_RemiCura/PROJETS/Postgres_Day_2014_10_RemiC/Point_Cloud/Image_Porcessing/building_border_detection/' ; 
 
 #we use gdal to read correctly the multiband tiff
 from osgeo import gdal ;
 gtif = gdal.Open( src_tif ) ;
-
-"""
-#transforming the raster to have bigger cell size
-    drv = gdal.GetDriverByName( 'GTiff' )
-    src_ds = gtif
-    
-    dst_ds = drv.Create(dest_folder+'resized_raster_1', src_ds.RasterXSize*2, src_ds.RasterYSize*2, gdal.GDT_Byte )
-    dst_ds.SetProjection(src_ds.GetProjectionRef())
-    dst_ds.SetGeoTransform(src_ds.GetGeoTransform())
-    
-    gdal.ReprojectImage( src_ds, dst_ds); 
-"""
-    
+ 
 
 """
 Now, we want to compute the relative height of point regarding the laser origin. So we perform : Z - Z_origin. In band term, it translates to 
@@ -49,9 +37,15 @@ Z = np.array(gtif.GetRasterBand(1).ReadAsArray()) ;
 Z_origin  = np.array(gtif.GetRasterBand(6).ReadAsArray()) ;
 Accum_points  = np.array(gtif.GetRasterBand(2).ReadAsArray()) ;
 
+#showing input :
+imshow(Z, cmap=plt.cm.gray) ;
+imshow(Z_origin, cmap=plt.cm.gray) ;  
+plt.show() ;
+
 #computing the realtiv height
-relativ_height = (Z-Z_origin) ;
-#relativ_height[relativ_height<0].size
+relativ_height = Z-Z_origin ;
+imshow(relativ_height, cmap=plt.cm.gray) ;  plt.show() ;
+#relativ_height[isnan(relativ_height)==False].size
 #filtering
 #keeping only relativ_height when more than 0 
 filter_relativ_height = relativ_height ;
@@ -84,37 +78,73 @@ plt.show()
 
 selem = disk(5) ;
 closed = binary_closing(data_for_skeleton, selem) ;
-imshow(closed, cmap=plt.cm.gray)
-plt.show()
+imshow(closed, cmap=plt.cm.gray) ; plt.show() ;
 closed[closed>0]=1 ;
 skeleton = skeletonize(data_for_skeleton)
-
-imshow(skeleton, cmap=plt.cm.gray)
-plt.show()
+imshow(skeleton, cmap=plt.cm.gray) ; plt.show() ;
 
 
+
+#trying to extract lines with hough transform
+from skimage.transform import (hough_line, hough_line_peaks,
+                               probabilistic_hough_line)
+lines = probabilistic_hough_line(skeleton, threshold=10, line_length=5, line_gap=20)
+for line in lines:
+    p0, p1 = line ;
+    plt.plot((p0[0], p1[0]), (p0[1], p1[1])) ;
+
+imshow(skeleton, cmap=plt.cm.gray) ; plt.show() ;
 #working on sidewalk :
-"""
-    We work on sidewalk : we use the relative height to compute the gradient 
-    of it. Then we perform thresholding then closing then straight sekeleton
+    """
+        We work on sidewalk : we use the relative height to compute the gradient 
+        of it. Then we perform thresholding then closing then straight sekeleton
+        NOte : the laser is approximately 2.50 meters above the street.
+        We are only interested in pixel where the height is between +-50cm from the ground
+            , to keep sidewalk.
+    """
 
-"""
-
-from skimage.morphology import disk
+from skimage.morphology import disk,square
 from skimage.filter.rank import gradient,threshold ;
-from skimage import img_as_float; 
+from skimage import img_as_float,img_as_uint,img_as_int; 
+from skimage import viewer ;
+from skimage.viewer.plugins import lineprofile ;
 
-#filtering the relative height again :
+#filtering the relative height again : we keep only pixel with relativ height between -3 and -2 meters, that is around the height of sidewalk + some margin
+
 filter_relativ_height_sidewalk = relativ_height ;
-filter_relativ_height_sidewalk[(filter_relativ_height_sidewalk<-3)&(filter_relativ_height_sidewalk>-2)] = 0 ;
-#filter_relativ_height_sidewalk[filter_relativ_height_sidewalk!=0] = 1 ;
-filter_relativ_height_sidewalk = filter_relativ_height_sidewalk.astype(bool);
+filter_relativ_height_sidewalk[(filter_relativ_height_sidewalk<-3)|(filter_relativ_height_sidewalk>-2)| isnan(filter_relativ_height_sidewalk)==True  ] = 0; 
+filter_relativ_height_sidewalk[ filter_relativ_height_sidewalk!=0] = 1
+#filter_relativ_height_sidewalk = (filter_relativ_height_sidewalk +2.5)*2 ;
+
+ 
+filter_relativ_height_sidewalk[(isnan(filter_relativ_height_sidewalk)==False)]
+imshow(filter_relativ_height_sidewalk, cmap=plt.cm.gray) ; plt.show();
+
+Z_around_sidewalk = Z * filter_relativ_height_sidewalk ; 
+imshow(Z_around_sidewalk, cmap=plt.cm.gray) ;
+#Z_around_sidewalk[Z_around_sidewalk!=0]
+new_viewer = viewer.ImageViewer(Z_around_sidewalk) ; new_viewer.show() ; 
 
 
-convert_to_float = img_as_float(filter_relativ_height_sidewalk) ;
-convert_to_float[convert_to_float!=0]
-grad = gradient(img_as_float(filter_relativ_height_sidewalk),disk(5)) ;
+convert_to_float = img_as_float(filter_relativ_height_sidewalk) 
+convert_to_float[isnan(convert_to_float)==False]
+
+convert_to_int = img_as_int(filter_relativ_height_sidewalk) ; 
+grad =  gradient(convert_to_int,disk(1)) ;
+
+
+
+new_viewer = viewer.ImageViewer(grad)
+new_viewer += lineprofile.LineProfile()
+new_viewer.show()
+
 imshow(grad, cmap=plt.cm.gray); plt.show();
+
+
+ 
+from skimage.morphology import disk
+from skimage.filter.rank import gradient
+  
 
 #outputting the raster to see if it worked :
     
