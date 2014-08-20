f87e515b6c7c314abf377e1b76784ad1f522638e
Remi C <remi.fake@fake.com>
2014-08-20 16:04:19 +0200

    proper stop in loop, refactoring with new parameter. Work but indexes of poitns cannot be used.

--- Point_Cloud/Patch_to_python/patch_to_python.sql
+++ Point_Cloud/Patch_to_python/patch_to_python.sql
@@ -65,54 +65,66 @@ SET search_path TO patch_to_python, benchmark, public;
 	WHERE --gid = 8480 --verticla patch
 		gid = 18875; --small patch 
 
-	  -- TABLE(support_points_indice INT[]  )  --, model float[])
 
+
+
+
+
+
+ 
 	--a plpython function taking the array of double precision and converting it to pointcloud, then looking for a plane inside.
 	--note that we could do the same to detect cylinder
-DROP FUNCTION IF EXISTS rc_py_point_array_to_numpy_array (iar FLOAT[]);
-CREATE FUNCTION rc_py_point_array_to_numpy_array (iar FLOAT[] ) RETURNS TABLE( support_point_index int[] , model FLOAT[])   
+DROP FUNCTION IF EXISTS rc_py_point_array_to_numpy_array ( FLOAT[],  INT,INT,  FLOAT,INT,FLOAT,INT);
+CREATE FUNCTION rc_py_point_array_to_numpy_array (
+	iar FLOAT[] 
+	,min_support_points INT DEFAULT 4
+	,max_plane_number INT DEFAULT 100
+	,_distance_threshold FLOAT DEFAULT 0.1
+	,_ksearch INT DEFAULT 50
+	,_distance_weight FLOAT DEFAULT 0.5 --between 0 and 1 . 
+	,_max_iterations INT DEFAULT 100 
+	) 
+RETURNS TABLE( support_point_index int[] , model FLOAT[])   
 AS $$
-
 """
-this function demonstrate how to convert input float[] into a numpy array, then 
+this function demonstrate how to convert input float[] into a numpy array
+then importing it into a pointcloud (pcl)
+then iteratively finding plan in the cloud using ransac
 """
 import numpy as np ;
 import pcl ; 
-#plpy.notice(iar) ; 
-#plpy.notice(type(iar)) ;
 
 #converting the 1D array to 2D array
 np_array = np.reshape(np.array(iar), (-1, 3)).astype(np.float32)  ; # note : we duplicate the data (copy), because we have to assume input data is read only
-#np_array_float32 = np_array.astype(np.float32)
-#plpy.notice(np_array) ; 
-#plpy.notice(type(np_array)) ;
 
-
-
+#importing this numpy array as pointcloud
 p = pcl.PointCloud() ;
 p.from_array(np_array) ;
 i= 0 ;
 result = list() ; 
-while i <3:  
-	plpy.notice(i) ;
+stop_condition = False; 
+indices = [0]*(min_support_points+1); 
+
+while ((len(indices) >= min_support_points) & (i<=max_plane_number) & (p.size>=min_support_points)):   
 	#prepare segmentation
-	seg = p.make_segmenter_normals(ksearch=10)
+	seg = p.make_segmenter_normals(ksearch=_ksearch)
 	seg.set_optimize_coefficients (True);
 	seg.set_model_type (pcl.SACMODEL_NORMAL_PLANE)
-	seg.set_normal_distance_weight (0.1)
+	seg.set_normal_distance_weight (_distance_weight) #Note : playing with this make the result more (0.5) or less(0.1) selective
 	seg.set_method_type (pcl.SAC_RANSAC)
-	seg.set_max_iterations (100)
-	seg.set_distance_threshold (0.1)
+	seg.set_max_iterations (_max_iterations)
+	seg.set_distance_threshold (_distance_threshold)
 	#segment
-	indices, model = seg.segment() 
-	result.append(   (indices, model) ) ;
+	indices, model = seg.segment()   
+	#writting result if it it satisfaying
+	if(len(indices) >= min_support_points) :
+		result.append(   (indices, model) ) ;
 	#indices, model = seg.segment() 
-	
+
 	#prepare next iteration
 	i+=1 ;
 	p =  p.extract(indices, negative=True) ; #removing from the cloud the points already used for this plan
-
-plpy.notice(result); 
+ 
 #print model
 #remaining_points= p.extract(indices, negative=True)
 
@@ -127,8 +139,9 @@ $$ LANGUAGE plpythonu;
 	
 	
 	SELECT gid, PC_NumPoints(patch) AS npoints, result.*
-	FROM riegl_pcpatch_space as rps,rc_patch_to_XYZ_array(patch) as arr , rc_py_point_array_to_numpy_array(arr) AS result
+	FROM riegl_pcpatch_space as rps,rc_patch_to_XYZ_array(patch) as arr , rc_py_point_array_to_numpy_array(arr,10,100,0.1) AS result
 	WHERE --gid = 8480 
 		--gid = 18875
 		--gid = 1598;
-		gid = 1051; -- a patch half hozirontal, half vertical 
+		--gid = 1051; -- a patch half hozirontal, half vertical . COntain several plans
+		gid = 1740 ; --a patch with a cylinder?
