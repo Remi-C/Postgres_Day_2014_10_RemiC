5c1e23c47fefaa7055a53ca6832aba1f093ed308
Remi C <remi.fake@fake.com>
2014-08-20 19:54:29 +0200

    a new module to be imported from, to better organyze code

---
+++ Point_Cloud/Patch_to_python/pointcloud_to_pcl.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 20 19:52:18 2014
+
+@author: remi
+
+This module group stuff to  be able to deal with converting from pointcloud 
+to PCL and to perform some basic stuff
+"""
+
