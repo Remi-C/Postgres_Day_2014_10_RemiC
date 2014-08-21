ba5cc61cc738198fb2fc6545c9089e932daa2f06
Remi C <remi.fake@fake.com>
2014-08-21 19:27:44 +0200

    working version with correct normalized output

--- Point_Cloud/Patch_to_python/PCA_ICA/PCA_ICA.sql
+++ Point_Cloud/Patch_to_python/PCA_ICA/PCA_ICA.sql
@@ -36,7 +36,7 @@ DROP FUNCTION IF EXISTS rc_py_pca_and_ica ( FLOAT[] );
 CREATE FUNCTION rc_py_pca_and_ica (
 	iar FLOAT[]  
 	) 
-RETURNS BOOLEAN 
+RETURNS TABLE( norm FLOAT,structural_vector float[])--, structural_vector float[]) 
 AS $$
 """ 
 	This function perform PCA/ICA and others to demonstrates analysis of structure of a vector of points(3D, but could be 2D)
@@ -47,8 +47,8 @@ import numpy as np ;
 from sklearn.decomposition import FastICA; 
 
 #converting the 1D array to 2D array (vector of 3D points)
-np_array = np.reshape(np.array(iar), (-1, 3)).astype(np.float32)  ; 
-
+np_array = np.reshape(np.array(iar), (-1, 3)); 
+np_array-=np.nanmax(np_array, axis=0) ;
 #performing ICA on the numpy array
 ica = FastICA(n_components=20
 	, algorithm='parallel'
@@ -57,20 +57,31 @@ ica = FastICA(n_components=20
 	, max_iter=200
 	, tol=0.0001  ) ;
 ica.fit(np_array) ;
+
+#normalizing the result
+norms =  np.linalg.norm(ica.mixing_,None,1) ;
+normalized_result = ica.mixing_ / norms[:, np.newaxis] ;
+
 #plpy.notice(points_fitted) ; 
-#plpy.notice(ica.mixing_) ; 
-return True ; 
+plpy.notice (normalized_result) ; 
+plpy.notice( np.linalg.norm(ica.mixing_,None,1)) ; 
+
+result = list() ; 
+for i in range(0,3):
+	result.append(( norms[i],(normalized_result[i]) ) ) ;  
+ 
+return result  ; 
 $$ LANGUAGE plpythonu IMMUTABLE STRICT; 
 
 	 -- testing querry  :
-		SELECT gid, PC_NumPoints(patch) AS npoints 
+		SELECT gid, PC_NumPoints(patch) AS npoints , result.*
 			
 		FROM riegl_pcpatch_space as rps
 			,rc_patch_to_XYZ_array(patch) as arr 
 			,   rc_py_pca_and_ica (
 				iar := arr
 				 ) AS result
-		WHERE -- gid = 8480 
+		WHERE  --gid = 8480 
 			--gid = 18875 -- very small patch
 			--gid = 1598 
 			--gid = 1051 -- a patch half hozirontal, half vertical . COntain several plans
