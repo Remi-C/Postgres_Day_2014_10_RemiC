ba2e5655ad94f4c970906797e6e4063aa9a8ed67
Remi C <remi.fake@fake.com>
2014-08-22 10:11:36 +0200

    working version, with export

--- Point_Cloud/Patch_to_python/PCA_ICA/PCA_ICA.sql
+++ Point_Cloud/Patch_to_python/PCA_ICA/PCA_ICA.sql
@@ -40,6 +40,8 @@ RETURNS TABLE( norm FLOAT,structural_vector float[])--, structural_vector float[
 AS $$
 """ 
 	This function perform PCA/ICA and others to demonstrates analysis of structure of a vector of points(3D, but could be 2D)
+	it returns 3 vectors, along with the norm of each vector.
+	The vector are the directions that explain best the data.
 """
 #importing needed modules
 import plpy ;
@@ -74,8 +76,7 @@ return result  ;
 $$ LANGUAGE plpythonu IMMUTABLE STRICT; 
 
 	 -- testing querry  :
-		SELECT gid, PC_NumPoints(patch) AS npoints , result.*
-			
+		SELECT gid, PC_NumPoints(patch) AS npoints , result.* 
 		FROM riegl_pcpatch_space as rps
 			,rc_patch_to_XYZ_array(patch) as arr 
 			,   rc_py_pca_and_ica (
@@ -85,6 +86,54 @@ $$ LANGUAGE plpythonu IMMUTABLE STRICT;
 			--gid = 18875 -- very small patch
 			--gid = 1598 
 			--gid = 1051 -- a patch half hozirontal, half vertical . COntain several plans
-			gid = 1740  --a patch with a cylinder? 
+			gid = 1740 ; --a patch with a cylinder?  
 	 
- 
+
+	--new function to be able to visualize the result in cloudcompare :
+	--we will materialize the vector by creating points all along. This way we can see something.
+	DROP FUNCTION IF EXISTS rc_patch_to_structural_vector_points(ipatch PCPATCH, i_patch_id INT);
+	CREATE OR REPLACE FUNCTION rc_patch_to_structural_vector_points(ipatch PCPATCH, i_patch_id INT )
+	  RETURNS TABLE  (X DOUBLE PRECISION,Y DOUBLE PRECISION , Z DOUBLE PRECISION
+		, patch_id INT, vector_id int, norm FLOAT)
+	  AS
+	$BODY$
+			--@brief this perform a ICA computing, then creates points along the dected vectors and output suitable data for visu in cloud comapre
+			-- @return :  a table holding the points along the vector with identification information
+			DECLARE 
+			BEGIN 
+
+			RETURN QUERY 
+			WITH ICA_result AS (	
+				SELECT i_patch_id AS patch_id
+					, ST_X(centroid) AS X_center, ST_Y(centroid) AS Y_center, (upper(z_int)+lower(z_int))/2 AS Z_center
+					,(row_number() over())::int AS vector_id
+					,   result.* 
+				FROM  (SELECT ipatch as pa) as pa,ST_Centroid( pa::geometry) as centroid
+				,rc_compute_range_for_a_patch( pa ,'Z') as z_int
+				,rc_patch_to_XYZ_array( pa) as arr 
+				,   rc_py_pca_and_ica (
+					iar := arr
+					 ) AS result 
+			)
+			--,generating_the_points AS (
+
+				SELECT round((X_center + structural_vector[1] * generate_series(1,100)* round(ica.norm::numeric,2)/100)::numeric,3)::float AS X  
+					,round((Y_center + structural_vector[2] * generate_series(1,100)* round(ica.norm::numeric,2)/100)::numeric,3)::float AS Y
+					,round((Z_center + structural_vector[3] * generate_series(1,100)* round(ica.norm::numeric,2)/100 )::numeric,3)::float AS Z
+					,ica.patch_id
+					, ica.vector_id
+					,ica.norm
+				FROM ICA_result AS ica ;
+			
+		 
+				RETURN; 
+			END ; 
+		$BODY$
+	LANGUAGE plpgsql IMMUTABLE STRICT;
+	--SELECT rc_patch_to_XYZ_array()
+
+
+	SELECT result.*
+	FROM riegl_pcpatch_space as rps
+		,rc_patch_to_structural_vector_points(rps.patch, rps.gid) AS result
+	WHERE gid = 1051; --1740	;
