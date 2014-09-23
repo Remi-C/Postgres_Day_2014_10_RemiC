71cd34502144183f0a599ded7be7c0dcc14d35a2
Remi C <remi.fake@fake.com>
2014-09-23 16:49:54 +0200

    file to give some order of magnitude on benchmark data

---
+++ Point_Cloud/IO/order_of_magnitude_on_benchmark.sql
@@ -0,0 +1,20 @@
+﻿set search_path to benchmark, public;
+
+select count(*)
+FROM riegl_pcpatch_space  
+--23996
+
+SELECT sum(PC_NumPoints(patch))
+FROM riegl_pcpatch_space  
+--12000000
+
+--pgadmin : 
+--table-size : 		10 Mo
+--toast table size : 	293 Mo
+-- index size : 		5.5 Mo
+
+--having 4 indexes :  
+	-- index on PC_Numpoints()
+	-- index on patch::geometry
+	-- index on ST_SetSRID(patch::geometry) 
+	-- index on   rc_compute_range_for_a_patch(patch, 'gps_time') ;
